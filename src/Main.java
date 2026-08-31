fe88ea09506c2558e981888c2d3a8f1f4d706e69
JayceDevelops <JayceIezzi@gmail.com>
2026-08-31 18:24:58 -0400

    built-in echo command

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,31 @@ public class Main {
             System.out.print("$ ");
             command = scanner.nextLine();
 
-            if (!"exit".equals(command)){
+            if ("exit".equals(command)){
+                break;
+            }
+
+            // Turns command into a tokens; tokens[0] = utility, tokens[1 - length] = arguments
+            String[] tokens = command.split(" ");
+
+            // Retrieves arguments
+            String[] arguments = new String[tokens.length - 1];
+            for (int i = 0; i < arguments.length; i++){
+                arguments[i] = tokens[i + 1];
+            }
+
+            // builtin echo command
+            if ("echo".equals(tokens[0])){
+                for (int i = 0; i < arguments.length; i++){
+                    if (i < arguments.length - 1){
+                        System.out.print(arguments[i] + " ");
+                    }
+                    else {
+                        System.out.println(arguments[i]);
+                    }
+                }
+            }
+            else {
                 System.out.println(command + ": command not found");
             }
             
